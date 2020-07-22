c0dd20f8c7f174c8485e1e54208a74d3207bd8b6
David Cairuz <davidcairuz@gmail.com>
2020-07-22 20:18:10 -0300

    Adicionando tudo

---
+++ seidel.py
@@ -0,0 +1,20 @@
+def seidel(A, b):
+    n = len(b)
+    x = np.random.rand(n)
+    k = np.random.rand(n)
+    
+    for _ in range(100):
+        for i in range(n):
+            sum_ = 0
+            for j in range(n):   
+                if i != j:
+                    sum_ += A[i, j] * x[j]
+            
+            x[i] = (b[i] - sum_)/A[i, i]
+        
+        if np.linalg.norm(x - k)/np.linalg.norm(x) < 1e-9:
+            break
+        else:
+            k = x.copy()
+            
+    return x
